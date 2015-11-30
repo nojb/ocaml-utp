463c263d45130069c8977dcdac1ad0f62b369015
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2015-11-30 15:16:34 +0100

    Rename

--- lib/utp.mli
+++ lib/utp.mli
@@ -39,4 +39,4 @@ val accept : context -> (socket * Unix.sockaddr) Lwt.t
 val read : socket -> bytes -> int -> int -> int Lwt.t
 val write : socket -> bytes -> int -> int -> int Lwt.t
 val close : socket -> unit Lwt.t
-val get_stats : socket -> socket_stats
+val get_socket_stats : socket -> socket_stats
